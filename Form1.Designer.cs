d1aa14ff6e9cf0dbbe30ebc24f1ec78d293b92dd
abdull435 <mabdulprvt@gmail.com>
2024-10-27 13:30:36 +0500

    panels add

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.mainpanel = new System.Windows.Forms.Panel();
+            this.addProductspanel = new System.Windows.Forms.Panel();
+            this.updateproductspanel = new System.Windows.Forms.Panel();
+            this.viewallpanel = new System.Windows.Forms.Panel();
+            this.returnpanel = new System.Windows.Forms.Panel();
+            this.recordpanel = new System.Windows.Forms.Panel();
+            this.billcheckpanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.addProductspanel.SuspendLayout();
+            this.updateproductspanel.SuspendLayout();
+            this.viewallpanel.SuspendLayout();
+            this.returnpanel.SuspendLayout();
+            this.recordpanel.SuspendLayout();
+            this.billcheckpanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // mainpanel
+            // 
+            this.mainpanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainpanel.Location = new System.Drawing.Point(0, 0);
+            this.mainpanel.Name = "mainpanel";
+            this.mainpanel.Size = new System.Drawing.Size(1924, 1050);
+            this.mainpanel.TabIndex = 0;
+            // 
+            // addProductspanel
+            // 
+            this.addProductspanel.Controls.Add(this.label3);
+            this.addProductspanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.addProductspanel.Location = new System.Drawing.Point(0, 0);
+            this.addProductspanel.Name = "addProductspanel";
+            this.addProductspanel.Size = new System.Drawing.Size(1924, 1050);
+            this.addProductspanel.TabIndex = 1;
+            // 
+            // updateproductspanel
+            // 
+            this.updateproductspanel.Controls.Add(this.label4);
+            this.updateproductspanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.updateproductspanel.Location = new System.Drawing.Point(0, 0);
+            this.updateproductspanel.Name = "updateproductspanel";
+            this.updateproductspanel.Size = new System.Drawing.Size(1924, 1050);
+            this.updateproductspanel.TabIndex = 2;
+            // 
+            // viewallpanel
+            // 
+            this.viewallpanel.Controls.Add(this.label6);
+            this.viewallpanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.viewallpanel.Location = new System.Drawing.Point(0, 0);
+            this.viewallpanel.Name = "viewallpanel";
+            this.viewallpanel.Size = new System.Drawing.Size(1924, 1050);
+            this.viewallpanel.TabIndex = 3;
+            // 
+            // returnpanel
+            // 
+            this.returnpanel.Controls.Add(this.label1);
+            this.returnpanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.returnpanel.Location = new System.Drawing.Point(0, 0);
+            this.returnpanel.Name = "returnpanel";
+            this.returnpanel.Size = new System.Drawing.Size(1924, 1050);
+            this.returnpanel.TabIndex = 4;
+            // 
+            // recordpanel
+            // 
+            this.recordpanel.Controls.Add(this.label2);
+            this.recordpanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.recordpanel.Location = new System.Drawing.Point(0, 0);
+            this.recordpanel.Name = "recordpanel";
+            this.recordpanel.Size = new System.Drawing.Size(1924, 1050);
+            this.recordpanel.TabIndex = 5;
+            // 
+            // billcheckpanel
+            // 
+            this.billcheckpanel.Controls.Add(this.label5);
+            this.billcheckpanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.billcheckpanel.Location = new System.Drawing.Point(0, 0);
+            this.billcheckpanel.Name = "billcheckpanel";
+            this.billcheckpanel.Size = new System.Drawing.Size(1924, 1050);
+            this.billcheckpanel.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(369, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Return";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(473, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Record check";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(477, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Add Product";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(452, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Update";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(411, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "bill check";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(499, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 20);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "View all";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1924, 1050);
+            this.Controls.Add(this.mainpanel);
+            this.Controls.Add(this.recordpanel);
+            this.Controls.Add(this.returnpanel);
+            this.Controls.Add(this.viewallpanel);
+            this.Controls.Add(this.updateproductspanel);
+            this.Controls.Add(this.addProductspanel);
+            this.Controls.Add(this.billcheckpanel);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Raja Shopping Center";
+            this.addProductspanel.ResumeLayout(false);
+            this.addProductspanel.PerformLayout();
+            this.updateproductspanel.ResumeLayout(false);
+            this.updateproductspanel.PerformLayout();
+            this.viewallpanel.ResumeLayout(false);
+            this.viewallpanel.PerformLayout();
+            this.returnpanel.ResumeLayout(false);
+            this.returnpanel.PerformLayout();
+            this.recordpanel.ResumeLayout(false);
+            this.recordpanel.PerformLayout();
+            this.billcheckpanel.ResumeLayout(false);
+            this.billcheckpanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel mainpanel;
+        private System.Windows.Forms.Panel addProductspanel;
+        private System.Windows.Forms.Panel updateproductspanel;
+        private System.Windows.Forms.Panel viewallpanel;
+        private System.Windows.Forms.Panel returnpanel;
+        private System.Windows.Forms.Panel recordpanel;
+        private System.Windows.Forms.Panel billcheckpanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
     }
 }
 
